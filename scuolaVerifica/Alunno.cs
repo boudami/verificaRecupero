86f00e9e816dc8eaacd22f7f09f11006a975e7e1
Amin Boudlale <AMIN.BOUDLALE@local.ittcesena.it>
2023-03-18 08:41:42 +0100

    Versione 1: metodo toString

---
+++ scuolaVerifica/Alunno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scuolaVerifica
+{
+    internal class Alunno
+    {
+        string nome, cognome, cittaResidenza;
+        int eta;
+
+        public Alunno(string nome, string cognome, string cittaResidenza, int eta)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.cittaResidenza = cittaResidenza;
+            this.eta = eta;
+        }
+        public string getNome() { return nome; }
+        public string getCognome() { return cognome; }
+        private object toString()
+        {
+            return getCognome;
+        }
+    }
+}
