b2e2c74d8ca4199fcb020a2e7487f3a8d68040a8
Amin Boudlale <AMIN.BOUDLALE@local.ittcesena.it>
2023-03-18 08:51:32 +0100

    Versione 2: dati alunno

---
+++ scuolaVerifica/Scuola.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scuolaVerifica
+{
+    internal class Scuola
+    {
+        string nome, indirizzo;
+        int orarioApertura, orarioChiusura;
+
+        public Scuola(string nome, string indirizzo, int orarioChiusura, int orarioApertura)
+        {
+            this.nome = nome;
+            this.indirizzo = indirizzo;
+            this.orarioChiusura = orarioChiusura;
+            this.orarioApertura = orarioApertura;
+        }
+    }
+}
